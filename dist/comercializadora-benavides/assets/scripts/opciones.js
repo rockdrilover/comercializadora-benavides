c4c43ce4461241f45c531371d32c8c9190973330
Aguinaga cuenca <s6745400@mexeo.bns>
2021-07-09 00:19:18 +0100

    Primer commit de la aplicacion

---
+++ dist/comercializadora-benavides/assets/scripts/opciones.js
@@ -0,0 +1,6 @@
+function hideMenu(){
+    divPrincipal.classList.remove('sidebar-mobile-open');
+    bottonMenu.classList.remove('is-active');
+    buttonR.classList.remove('header-mobile-open');
+}
+
